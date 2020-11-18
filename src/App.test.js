4613f52e9390cede761c42fee7d299286b4a413b
Kenchir <kchirchir.dev@gmail.com>
2020-11-18 11:04:40 +0300

    \ctest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Predict Loan/i);
+  console.log(linkElement);
   expect(linkElement).toBeInTheDocument();
 });
