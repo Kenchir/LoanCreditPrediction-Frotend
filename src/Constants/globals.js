67f291dd4db58cf7f889b48b763b2ad424ee4784
Kenchir <kchirchir.dev@gmail.com>
2020-11-23 10:52:27 +0300

    production url build

--- src/Constants/globals.js
+++ src/Constants/globals.js
@@ -1,12 +1,12 @@
 const prod = {
   url: {
-    API_URL: process.env.REACT_APP_API_URL,
+    API_URL: process.env.REACT_APP_PROD_API_URL,
   },
 };
 
 const dev = {
   url: {
-    API_URL: "http://localhost:8080",
+    API_URL: process.env.REACT_APP_DEV_API_URL,
   },
 };
 export const globals = process.env.NODE_ENV === "production" ? prod : dev;
